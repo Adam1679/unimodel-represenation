fbc179202390f3a98de10bbc44bf01610d3bfb90
Ubuntu <ubuntu@ip-172-31-71-137.ec2.internal>
2020-10-11 10:14:13 +0000

    update faster-rcnn(trained on coco2017)

--- image/download_model.sh
+++
@@ -1,5 +0,0 @@
-fileId=1loIh6AYygDALfID1_Ou2z6AwQB1MEjOY
-fileName=fasterrcnn_12211511_0.701052458187_torchvision_pretrain.pth
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
-code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
